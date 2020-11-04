87b006e413697b7e6f8ccd6fcd36edcc097d676b
Alaa-Omran <alaa.omran@nana.sa>
2020-11-05 01:29:06 +0200

    first code

---
+++ src/test/java/qaclickacademy/mavenjava/SeleiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleiumTest {
+	
+	
+	
+
+	@Test
+	public void browserTest()
+	{
+		System.out.println("browserTest");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+
+}
